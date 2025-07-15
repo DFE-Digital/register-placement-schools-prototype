7991d90a087679458a87f9491a2afe6e1183a576
Simon Whatley <simonwhatley@gmail.com>
2025-07-15 15:23:15 +0100

    Update activity log migration

--- app/migrations/20250715121200-create-activity-log.js
+++ app/migrations/20250715121200-create-activity-log.js
@@ -44,6 +44,16 @@ module.exports = {
         type: Sequelize.DATE,
         allowNull: false,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       }
     })
   },
