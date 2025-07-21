de00085e68f21f8942777f0d16050d1a197416ed
Simon Whatley <simonwhatley@gmail.com>
2025-07-21 15:43:04 +0100

    Create school model

---
+++ app/models/school.js
@@ -0,0 +1,167 @@
+const { Model, DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+  class School extends Model {
+    static associate(models) {
+      School.hasOne(models.SchoolAddress, {
+        foreignKey: 'schoolId',
+        as: 'schoolAddress'
+      })
+
+      School.hasOne(models.SchoolDetail, {
+        foreignKey: 'schoolId',
+        as: 'schoolDetail'
+      })
+
+      School.belongsTo(models.SchoolType, {
+        foreignKey: 'type',
+        targetKey: 'code',
+        as: 'schoolType'
+      })
+
+      School.belongsTo(models.SchoolGroup, {
+        foreignKey: 'group',
+        targetKey: 'code',
+        as: 'schoolGroup'
+      })
+
+      School.belongsTo(models.SchoolStatus, {
+        foreignKey: 'status',
+        targetKey: 'code',
+        as: 'schoolStatus'
+      })
+
+      School.belongsTo(models.SchoolEducationPhase, {
+        foreignKey: 'educationPhase',
+        targetKey: 'code',
+        as: 'schoolEducationPhase'
+      })
+
+      School.belongsTo(models.User, {
+        foreignKey: 'createdById',
+        as: 'createdByUser'
+      })
+
+      School.belongsTo(models.User, {
+        foreignKey: 'updatedById',
+        as: 'updatedByUser'
+      })
+    }
+  }
+
+  School.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      urn: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      ukprn: {
+        type: DataTypes.STRING
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      group: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      educationPhase: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'education_phase',
+        validate: {
+          notEmpty: true
+        }
+      },
+      website: {
+        type: DataTypes.STRING
+      },
+      telephone: {
+        type: DataTypes.STRING
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: 'created_at'
+      },
+      createdById: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: 'created_by_id'
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: 'updated_at'
+      },
+      updatedById: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: 'updated_by_id'
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        field: 'deleted_at'
+      },
+      deletedById: {
+        type: DataTypes.UUID,
+        field: 'deleted_by_id'
+      }
+    },
+    {
+      sequelize,
+      modelName: 'School',
+      tableName: 'schools',
+      timestamps: true
+    }
+  )
+
+  // const createRevisionHook = require('../hooks/revisionHook')
+
+  // School.addHook('afterCreate', (instance, options) =>
+  //   createRevisionHook({ revisionModelName: 'SchoolRevision', modelKey: 'school' })(instance, {
+  //     ...options,
+  //     hookName: 'afterCreate'
+  //   })
+  // )
+
+  // School.addHook('afterUpdate', (instance, options) => {
+  //   const hookName = instance.deletedById !== null ? 'afterDestroy' : 'afterUpdate'
+  //   createRevisionHook({ revisionModelName: 'SchoolRevision', modelKey: 'school' })(instance, {
+  //     ...options,
+  //     hookName
+  //   })
+  // })
+
+  return School
+}
