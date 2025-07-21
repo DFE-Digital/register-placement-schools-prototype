9c55bbbb3d7ecacaa279c44f85975ae856b1c903
Simon Whatley <simonwhatley@gmail.com>
2025-07-21 15:43:38 +0100

    Create schoolEducationPhase model

---
+++ app/models/schoolEducationPhase.js
@@ -0,0 +1,96 @@
+const { Model, DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+  class SchoolEducationPhase extends Model {
+    static associate(models) {
+      SchoolEducationPhase.hasMany(models.School, {
+        foreignKey: 'educationPhase',
+        sourceKey: 'code',
+        as: 'schools'
+      })
+
+      SchoolEducationPhase.belongsTo(models.User, {
+        foreignKey: 'createdById',
+        as: 'createdByUser'
+      })
+
+      SchoolEducationPhase.belongsTo(models.User, {
+        foreignKey: 'updatedById',
+        as: 'updatedByUser'
+      })
+    }
+  }
+
+  SchoolEducationPhase.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      code:  {
+        type: DataTypes.STRING
+      },
+      name:  {
+        type: DataTypes.STRING
+      },
+      rank:  {
+        type: DataTypes.TINYINT
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: 'created_at'
+      },
+      createdById: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: 'created_by_id'
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: 'updated_at'
+      },
+      updatedById: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: 'updated_by_id'
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        field: 'deleted_at'
+      },
+      deletedById: {
+        type: DataTypes.UUID,
+        field: 'deleted_by_id'
+      }
+    },
+    {
+      sequelize,
+      modelName: 'SchoolEducationPhase',
+      tableName: 'school_statuses',
+      timestamps: true
+    }
+  )
+
+  // const createRevisionHook = require('../hooks/revisionHook')
+
+  // SchoolEducationPhase.addHook('afterCreate', (instance, options) =>
+  //   createRevisionHook({ revisionModelName: 'SchoolEducationPhaseRevision', modelKey: 'schoolEducationPhase' })(instance, {
+  //     ...options,
+  //     hookName: 'afterCreate'
+  //   })
+  // )
+
+  // SchoolEducationPhase.addHook('afterUpdate', (instance, options) => {
+  //   const hookName = instance.deletedById !== null ? 'afterDestroy' : 'afterUpdate'
+  //   createRevisionHook({ revisionModelName: 'SchoolEducationPhaseRevision', modelKey: 'schoolEducationPhase' })(instance, {
+  //     ...options,
+  //     hookName
+  //   })
+  // })
+
+  return SchoolEducationPhase
+}
