bd096d525545afb1b60232d541ce4d5d3759e0fc
Simon Whatley <simonwhatley@gmail.com>
2025-07-28 14:54:01 +0100

    Create schoolReligiousCharacter.js

---
+++ app/models/schoolReligiousCharacter.js
@@ -0,0 +1,96 @@
+const { Model, DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+  class SchoolReligiousCharacter extends Model {
+    static associate(models) {
+      SchoolReligiousCharacter.hasMany(models.SchoolDetail, {
+        foreignKey: 'religiousCharacterCode',
+        sourceKey: 'code',
+        as: 'schoolDetails'
+      })
+
+      SchoolReligiousCharacter.belongsTo(models.User, {
+        foreignKey: 'createdById',
+        as: 'createdByUser'
+      })
+
+      SchoolReligiousCharacter.belongsTo(models.User, {
+        foreignKey: 'updatedById',
+        as: 'updatedByUser'
+      })
+    }
+  }
+
+  SchoolReligiousCharacter.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      code:  {
+        type: DataTypes.STRING
+      },
+      name:  {
+        type: DataTypes.STRING
+      },
+      rank:  {
+        type: DataTypes.TINYINT
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: 'created_at'
+      },
+      createdById: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: 'created_by_id'
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: 'updated_at'
+      },
+      updatedById: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: 'updated_by_id'
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        field: 'deleted_at'
+      },
+      deletedById: {
+        type: DataTypes.UUID,
+        field: 'deleted_by_id'
+      }
+    },
+    {
+      sequelize,
+      modelName: 'SchoolReligiousCharacter',
+      tableName: 'school_religious_characters',
+      timestamps: true
+    }
+  )
+
+  // const createRevisionHook = require('../hooks/revisionHook')
+
+  // SchoolReligiousCharacter.addHook('afterCreate', (instance, options) =>
+  //   createRevisionHook({ revisionModelName: 'SchoolReligiousCharacterRevision', modelKey: 'schoolReligiousCharacter' })(instance, {
+  //     ...options,
+  //     hookName: 'afterCreate'
+  //   })
+  // )
+
+  // SchoolReligiousCharacter.addHook('afterUpdate', (instance, options) => {
+  //   const hookName = instance.deletedById !== null ? 'afterDestroy' : 'afterUpdate'
+  //   createRevisionHook({ revisionModelName: 'SchoolReligiousCharacterRevision', modelKey: 'schoolReligiousCharacter' })(instance, {
+  //     ...options,
+  //     hookName
+  //   })
+  // })
+
+  return SchoolReligiousCharacter
+}
