2c37e9cb512ac21e739a82a9a7ad56e88ee66346
Simon Whatley <simonwhatley@gmail.com>
2025-07-30 16:14:57 +0100

    Remove unneeded controllers

--- app/controllers/location.js
+++
@@ -1,9 +0,0 @@
-exports.locationSuggestions_json = async (req, res) => {
-    req.headers['Access-Control-Allow-Origin'] = true
-
-    const query = req.query.search || ''
-
-    const locations = []
-
-    res.json(locations)
-}
