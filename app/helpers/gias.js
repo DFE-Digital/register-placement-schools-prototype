037a07e4e7ab9cf3ba2159b42d6dc1d288e31470
Simon Whatley <simonwhatley@gmail.com>
2025-08-01 14:13:16 +0100

    Update GIAS helper with additional contrast factors

--- app/helpers/gias.js
+++ app/helpers/gias.js
@@ -1,10 +1,166 @@
 const {
-  SchoolType,
+  SchoolAdmissionsPolicy,
+  SchoolBoarder,
+  SchoolEducationPhase,
+  SchoolGender,
   SchoolGroup,
+  SchoolNurseryProvision,
+  SchoolReligiousCharacter,
+  SchoolSixthForm,
+  SchoolSpecialClass,
   SchoolStatus,
-  SchoolEducationPhase
+  SchoolType,
+  SchoolUrbanRuralLocation
 } = require('../models')
 
+
+const getSchoolAdmissionsPolicyOptions = async () => {
+  const items = []
+
+  const schoolAdmissionsPolicies = await SchoolAdmissionsPolicy.findAll({
+    order: [
+      ['rank', 'ASC'],
+      ['name', 'ASC']
+    ]
+  })
+
+  schoolAdmissionsPolicies.forEach((schoolAdmissionsPolicy) => {
+    const item = {}
+    item.text = schoolAdmissionsPolicy.name
+    item.value = schoolAdmissionsPolicy.code
+    item.id = schoolAdmissionsPolicy.id
+    items.push(item)
+  })
+
+  return items
+}
+
+const getSchoolAdmissionsPolicyLabel = async (code) => {
+  let label
+
+  const schoolAdmissionsPolicy = await SchoolAdmissionsPolicy.findOne({ where: {
+    code: code
+  }})
+
+  if (schoolAdmissionsPolicy) {
+    label = schoolAdmissionsPolicy.name
+  } else {
+    label = code
+  }
+
+  return label
+}
+
+const getSchoolBoarderOptions = async () => {
+  const items = []
+
+  const schoolBoarders = await SchoolBoarder.findAll({
+    order: [
+      ['rank', 'ASC'],
+      ['name', 'ASC']
+    ]
+  })
+
+  schoolBoarders.forEach((schoolBoarder) => {
+    const item = {}
+    item.text = schoolBoarder.name
+    item.value = schoolBoarder.code
+    item.id = schoolBoarder.id
+    items.push(item)
+  })
+
+  return items
+}
+
+const getSchoolBoarderLabel = async (code) => {
+  let label
+
+  const schoolBoarder = await SchoolBoarder.findOne({ where: {
+    code: code
+  }})
+
+  if (schoolBoarder) {
+    label = schoolBoarder.name
+  } else {
+    label = code
+  }
+
+  return label
+}
+
+const getSchoolEducationPhaseOptions = async () => {
+  const items = []
+
+  const schoolEducationPhases = await SchoolEducationPhase.findAll({
+    order: [
+      ['rank', 'ASC'],
+      ['name', 'ASC']
+    ]
+  })
+
+  schoolEducationPhases.forEach((schoolEducationPhase) => {
+    const item = {}
+    item.text = schoolEducationPhase.name
+    item.value = schoolEducationPhase.code
+    item.id = schoolEducationPhase.id
+    items.push(item)
+  })
+
+  return items
+}
+
+const getSchoolEducationPhaseLabel = async (code) => {
+  let label
+
+  const schoolEducationPhase = await SchoolEducationPhase.findOne({ where: {
+    code: code
+  }})
+
+  if (schoolEducationPhase) {
+    label = schoolEducationPhase.name
+  } else {
+    label = code
+  }
+
+  return label
+}
+
+const getSchoolGenderOptions = async () => {
+  const items = []
+
+  const schoolGenders = await SchoolGender.findAll({
+    order: [
+      ['rank', 'ASC'],
+      ['name', 'ASC']
+    ]
+  })
+
+  schoolGenders.forEach((schoolGender) => {
+    const item = {}
+    item.text = schoolGender.name
+    item.value = schoolGender.code
+    item.id = schoolGender.id
+    items.push(item)
+  })
+
+  return items
+}
+
+const getSchoolGenderLabel = async (code) => {
+  let label
+
+  const schoolGender = await SchoolGender.findOne({ where: {
+    code: code
+  }})
+
+  if (schoolGender) {
+    label = schoolGender.name
+  } else {
+    label = code
+  }
+
+  return label
+}
 
 const getSchoolGroupOptions = async () => {
   const items = []
@@ -36,6 +192,154 @@ const getSchoolGroupLabel = async (code) => {
 
   if (schoolGroup) {
     label = schoolGroup.name
+  } else {
+    label = code
+  }
+
+  return label
+}
+
+const getSchoolNurseryProvisionOptions = async () => {
+  const items = []
+
+  const schoolNurseryProvisions = await SchoolNurseryProvision.findAll({
+    order: [
+      ['rank', 'ASC'],
+      ['name', 'ASC']
+    ]
+  })
+
+  schoolNurseryProvisions.forEach((schoolNurseryProvision) => {
+    const item = {}
+    item.text = schoolNurseryProvision.name
+    item.value = schoolNurseryProvision.code
+    item.id = schoolNurseryProvision.id
+    items.push(item)
+  })
+
+  return items
+}
+
+const getSchoolNurseryProvisionLabel = async (code) => {
+  let label
+
+  const schoolNurseryProvision = await SchoolNurseryProvision.findOne({ where: {
+    code: code
+  }})
+
+  if (schoolNurseryProvision) {
+    label = schoolNurseryProvision.name
+  } else {
+    label = code
+  }
+
+  return label
+}
+
+const getSchoolReligiousCharacterOptions = async () => {
+  const items = []
+
+  const schoolReligiousCharacters = await SchoolReligiousCharacter.findAll({
+    order: [
+      ['rank', 'ASC'],
+      ['name', 'ASC']
+    ]
+  })
+
+  schoolReligiousCharacters.forEach((schoolReligiousCharacter) => {
+    const item = {}
+    item.text = schoolReligiousCharacter.name
+    item.value = schoolReligiousCharacter.code
+    item.id = schoolReligiousCharacter.id
+    items.push(item)
+  })
+
+  return items
+}
+
+const getSchoolReligiousCharacterLabel = async (code) => {
+  let label
+
+  const schoolReligiousCharacter = await SchoolReligiousCharacter.findOne({ where: {
+    code: code
+  }})
+
+  if (schoolReligiousCharacter) {
+    label = schoolReligiousCharacter.name
+  } else {
+    label = code
+  }
+
+  return label
+}
+
+const getSchoolSixthFormOptions = async () => {
+  const items = []
+
+  const schoolSixthForms = await SchoolSixthForm.findAll({
+    order: [
+      ['rank', 'ASC'],
+      ['name', 'ASC']
+    ]
+  })
+
+  schoolSixthForms.forEach((schoolSixthForm) => {
+    const item = {}
+    item.text = schoolSixthForm.name
+    item.value = schoolSixthForm.code
+    item.id = schoolSixthForm.id
+    items.push(item)
+  })
+
+  return items
+}
+
+const getSchoolSixthFormLabel = async (code) => {
+  let label
+
+  const schoolSixthForm = await SchoolSixthForm.findOne({ where: {
+    code: code
+  }})
+
+  if (schoolSixthForm) {
+    label = schoolSixthForm.name
+  } else {
+    label = code
+  }
+
+  return label
+}
+
+const getSchoolSpecialClassOptions = async () => {
+  const items = []
+
+  const schoolSpecialClasses = await SchoolSpecialClass.findAll({
+    order: [
+      ['rank', 'ASC'],
+      ['name', 'ASC']
+    ]
+  })
+
+  schoolSpecialClasses.forEach((schoolSpecialClass) => {
+    const item = {}
+    item.text = schoolSpecialClass.name
+    item.value = schoolSpecialClass.code
+    item.id = schoolSpecialClass.id
+    items.push(item)
+  })
+
+  return items
+}
+
+const getSchoolSpecialClassLabel = async (code) => {
+  let label
+
+  const schoolSpecialClass = await SchoolSpecialClass.findOne({ where: {
+    code: code
+  }})
+
+  if (schoolSpecialClass) {
+    label = schoolSpecialClass.name
   } else {
     label = code
   }
@@ -117,36 +421,36 @@ const getSchoolStatusLabel = async (code) => {
   return label
 }
 
-const getSchoolEducationPhaseOptions = async () => {
+const getSchoolUrbanRuralLocationOptions = async () => {
   const items = []
 
-  const schoolEducationPhases = await SchoolEducationPhase.findAll({
+  const schoolUrbanRuralLocations = await SchoolUrbanRuralLocation.findAll({
     order: [
       ['rank', 'ASC'],
       ['name', 'ASC']
     ]
   })
 
-  schoolEducationPhases.forEach((schoolEducationPhase) => {
+  schoolUrbanRuralLocations.forEach((schoolUrbanRuralLocation) => {
     const item = {}
-    item.text = schoolEducationPhase.name
-    item.value = schoolEducationPhase.code
-    item.id = schoolEducationPhase.id
+    item.text = schoolUrbanRuralLocation.name
+    item.value = schoolUrbanRuralLocation.code
+    item.id = schoolUrbanRuralLocation.id
     items.push(item)
   })
 
   return items
 }
 
-const getSchoolEducationPhaseLabel = async (code) => {
+const getSchoolUrbanRuralLocationLabel = async (code) => {
   let label
 
-  const schoolEducationPhase = await SchoolEducationPhase.findOne({ where: {
+  const schoolUrbanRuralLocation = await SchoolUrbanRuralLocation.findOne({ where: {
     code: code
   }})
 
-  if (schoolEducationPhase) {
-    label = schoolEducationPhase.name
+  if (schoolUrbanRuralLocation) {
+    label = schoolUrbanRuralLocation.name
   } else {
     label = code
   }
@@ -155,12 +459,28 @@ const getSchoolEducationPhaseLabel = async (code) => {
 }
 
 module.exports = {
-  getSchoolTypeOptions,
-  getSchoolTypeLabel,
-  getSchoolGroupOptions,
-  getSchoolGroupLabel,
-  getSchoolStatusOptions,
-  getSchoolStatusLabel,
+  getSchoolAdmissionsPolicyLabel,
+  getSchoolAdmissionsPolicyOptions,
+  getSchoolBoarderLabel,
+  getSchoolBoarderOptions,
+  getSchoolEducationPhaseLabel,
   getSchoolEducationPhaseOptions,
-  getSchoolEducationPhaseLabel
+  getSchoolGenderLabel,
+  getSchoolGenderOptions,
+  getSchoolGroupLabel,
+  getSchoolGroupOptions,
+  getSchoolNurseryProvisionLabel,
+  getSchoolNurseryProvisionOptions,
+  getSchoolReligiousCharacterLabel,
+  getSchoolReligiousCharacterOptions,
+  getSchoolSixthFormLabel,
+  getSchoolSixthFormOptions,
+  getSchoolSpecialClassLabel,
+  getSchoolSpecialClassOptions,
+  getSchoolStatusLabel,
+  getSchoolStatusOptions,
+  getSchoolTypeLabel,
+  getSchoolTypeOptions,
+  getSchoolUrbanRuralLocationLabel,
+  getSchoolUrbanRuralLocationOptions
 }
