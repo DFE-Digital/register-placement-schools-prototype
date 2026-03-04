3ea5f2863c14b819a50116a1f92735a442e7a3ea
Simon Whatley <simonwhatley@gmail.com>
2026-03-04 21:33:04 +0000

    Add school relationship models

---
+++ app/models/schoolRelationshipType.js
@@ -0,0 +1,76 @@
+const { Model, DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+  class SchoolRelationshipType extends Model {
+    static associate(models) {
+      SchoolRelationshipType.hasMany(models.SchoolRelationship, {
+        foreignKey: 'relationshipTypeId',
+        as: 'relationships'
+      })
+
+      SchoolRelationshipType.belongsTo(models.User, {
+        foreignKey: 'createdById',
+        as: 'createdByUser'
+      })
+
+      SchoolRelationshipType.belongsTo(models.User, {
+        foreignKey: 'updatedById',
+        as: 'updatedByUser'
+      })
+    }
+  }
+
+  SchoolRelationshipType.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      description: {
+        type: DataTypes.STRING
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: 'created_at'
+      },
+      createdById: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: 'created_by_id'
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: 'updated_at'
+      },
+      updatedById: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: 'updated_by_id'
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        field: 'deleted_at'
+      },
+      deletedById: {
+        type: DataTypes.UUID,
+        field: 'deleted_by_id'
+      }
+    },
+    {
+      sequelize,
+      modelName: 'SchoolRelationshipType',
+      tableName: 'school_relationship_types',
+      timestamps: true
+    }
+  )
+
+  return SchoolRelationshipType
+}
